cd5ac3539b62ea6576a3ba9d85bf38a8b3fe9b7e
Mayank Sanghi <msanghi.cdac@gmail.com>
2026-06-06 13:19:19 +0400

    this will be the only file for the project

--- mini_gpt.py
+++ mini_gpt.py
@@ -5,8 +5,8 @@ with open("./data/tiny_corpus.txt", "r", encoding="utf-8") as f:
     corpus = f.read()
 # print(corpus)
 
-token = tiktoken.encoding_for_model("gpt-4o")
+token = tiktoken.encoding_for_model("text-embedding-3-small")
 
 tokens  = token.encode(corpus)
 print(tokens)
-print(token.decode(tokens))
+# print(token.decode(tokens))
