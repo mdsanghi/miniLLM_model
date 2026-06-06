82b5f0048995b4c66745549f1e9b7a66e66ee133
Manjusha Deore <manjusha.deore2525@gmail.com>
2026-06-06 15:20:22 +0530

    Addition code

---
+++ practice_harshit.py
@@ -0,0 +1,9 @@
+# Input two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("Sum =", sum)
