d440175cbb63bc5daccc2201111242c62835cd77
Manjusha Deore <manjusha.deore2525@gmail.com>
2026-06-12 01:00:08 +0530

    Token Embedding and positional embedding

--- practice_manju.py
+++ practice_manju.py
@@ -1,5 +1,6 @@
 import tiktoken
-
+import torch
+import torch.nn as nn
 #Tokenization
 with open("./data/tiny_corpus.txt", "r", encoding="utf-8") as f:
     corpus = f.read()
@@ -13,7 +14,32 @@ with open("./data/tiny_corpus.txt", "r", encoding="utf-8") as f:
     print(token_ids)
     print(tokenizer.decode(token_ids))
     print("No of tokens:",len(token_ids))
+ 
+    vocab_size = tokenizer.n_vocab 
+    data = torch.tensor(token_ids, dtype=torch.long)
+    print(f"Dataset Shape:",data.shape)
+    
     
     # Token Embedding
     
+    d_model=256
+    token_embedded = nn.Embedding(vocab_size, d_model)
+    block_size = 64
+    x_tokens = torch.tensor(token_ids[:block_size], dtype=torch.long)
+
+    print(f"\nInput token IDs (Shape: {list(x_tokens.shape)}):")
+    print(x_tokens)
+    X_token_embedded = token_embedded(x_tokens)
+
+    print(f"\nOutput embedded tensor shape: {list(X_token_embedded.shape)}")
+    print("First token's vector snippet (first 5 elements):")
+    print(X_token_embedded[0][:5].tolist())
     
+    #Positional Embedding
+    pos_embedding = nn.Embedding(block_size, d_model)
+    positions = torch.arange(block_size)
+    print(f"Position Indices: {positions.tolist()}")
+    X_pos_embedded = pos_embedding(positions)
+    print(f"Positional Embedding Shape: {list(X_pos_embedded.shape)}")
+    X_final = X_token_embedded + X_pos_embedded
+    print(f"Final Combined Embedding Shape: {list(X_final.shape)}")
